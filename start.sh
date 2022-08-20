9ac67902b71b85c6335ed7aa40d9edc90296cfab
M. Andrew Eick <maeick@missionfocus.com>
2022-08-20 12:20:43 -0400

    updates for EDC

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # The `--accept-dns=false` argument ensures that Pi-hole does NOT get configured to use itself
 # for DNS resolution, creating a circular dependency.
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${FLY_APP_NAME} --accept-dns=false
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${FLY_APP_NAME} --accept-dns=false --advertise-tags=tag:edc
 
 # This is a silly hack to ensure the container doesn't stop after `start.sh` is done running.
 # See https://fly.io/docs/app-guides/multiple-processes/
